964d4367de73d09c99060c81da7d98f5238f3092
Artyom Kazak <yom@artyom.me>
2014-09-21 22:58:56 +0400

    Set up customisation.

--- init.el
+++ init.el
@@ -1,1 +1,6 @@
 (add-to-list 'load-path user-emacs-directory)
+
+;; Load “customize”d variables.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))

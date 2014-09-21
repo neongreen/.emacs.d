9a3b16612916f5ce161629549b9cb97f7fdfc4ba
Artyom Kazak <yom@artyom.me>
2014-09-21 23:17:44 +0400

    Add custom keybindings module.

--- init.el
+++ init.el
@@ -4,3 +4,6 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
+
+;; Load custom keybindings.
+(require 'keys)
---
+++ keys.el
@@ -0,0 +1,14 @@
+(defvar my-keys-minor-mode-map (make-keymap) "my-keys-minor-mode keymap.")
+
+(define-minor-mode my-keys-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  t " my-keys" 'my-keys-minor-mode-map)
+
+(my-keys-minor-mode 1)
+
+(defun my-minibuffer-setup-hook ()
+  (my-keys-minor-mode 0))
+
+(add-hook 'minibuffer-setup-hook 'my-minibuffer-setup-hook)
+
+(provide 'keys)

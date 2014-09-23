caffdfd7c5f0c8845b6aef28f4093f102a5bec78
Artyom Kazak <yom@artyom.me>
2014-09-23 20:22:10 +0400

    Highlight comments better.

--- custom.el
+++ custom.el
@@ -14,4 +14,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-comment-face ((t (:background "#e0e0e0"))))
+ '(font-lock-doc-face ((t (:background "#ceebff")))))

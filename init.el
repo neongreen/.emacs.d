de0a8ef5228e1ca02dd4ca781cad381ab48a7f33
Artyom Kazak <yom@artyom.me>
2014-09-23 19:17:35 +0400

    Initialise package system.

--- init.el
+++ init.el
@@ -1,5 +1,11 @@
 (add-to-list 'load-path user-emacs-directory)
 
+;; Initialise package system.
+(require 'package)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
 ;; Load “customize”d variables.
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)

fb910d55b4cd1dbda95ab463f7d97d859257f530
Artyom <yom@artyom.me>
2014-09-26 21:26:04 +0400

    Write a Haskell-mode comment.

--- haskell.el
+++ haskell.el
@@ -1,3 +1,15 @@
+;; This enables Haskell-mode. Useful tips:
+;;
+;;   * [C-c C-b] shows the REPL buffer.
+;;
+;;   * To enable tag generation, you have to install hasktags from Hackage.
+;;     After that you will be able to instantly jump to function definitions
+;;     with [M-.].
+;;
+;;   * [C-c C-t] shows type of function at point.
+;;
+;;   * [M-x imenu] provides an index of functions defined in module.
+
 (require 'haskell-mode)
 
 (add-to-list 'exec-path "/home/yom/.cabal/bin/")

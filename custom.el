d2bd58b261e8a784163435b933e153ab9f0b60e7
Artyom <yom@artyom.me>
2015-03-19 12:22:50 +0300

    Enable solarized-light, too

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
  '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(dired-auto-revert-buffer t)
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(electric-indent-mode nil)

edc8ea46816ebac7fff56a4a4d3dd96e416737a8
Artyom <yom@artyom.me>
2015-03-19 12:38:51 +0300

    Default font

--- custom.el
+++ custom.el
@@ -40,6 +40,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 102 :width normal))))
  '(font-lock-comment-face ((t (:inherit highlight))))
  '(font-lock-doc-face ((t (:foreground "#6c71c4" :slant normal))))
  '(variable-pitch ((t (:height 1.225 :family "Sans")))))

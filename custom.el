e03f83e67c3fac7da574fa2c3f5bcd174a2a588d
Artyom Kazak <yom@artyom.me>
2018-09-23 19:53:27 +0200

    dabbrev stuff

--- custom.el
+++ custom.el
@@ -13,6 +13,10 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(dabbrev-case-distinction nil)
+ '(dabbrev-case-fold-search nil)
+ '(dabbrev-case-replace nil)
+ '(dabbrev-check-all-buffers nil)
  '(delete-old-versions t)
  '(dired-auto-revert-buffer t)
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))

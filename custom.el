f025052ccb1ec5c6c2b33371b626ea89662d483e
Artyom Kazak <yom@artyom.me>
2018-09-23 19:51:10 +0200

    ag stuff

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ag-arguments (quote ("--smart-case")))
+ '(ag-arguments (quote ("--smart-case" "--hidden" "--ignore" ".git")))
  '(ag-highlight-search t)
  '(backup-by-copying t)
  '(backup-directory-alist (quote (("." . "~/.saves"))))

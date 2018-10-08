354573a15579f0e7ae88582d6f22e86b4671be83
Artyom Kazak <yom@artyom.me>
2018-10-08 11:06:14 +0200

    No ringing

--- custom.el
+++ custom.el
@@ -120,6 +120,7 @@
     (ace-jump-mode adoc-mode ag auto-complete auto-indent-mode avy browse-kill-ring+ coffee-mode counsel counsel-gtags counsel-projectile crux csv-mode dockerfile-mode dumb-jump elm-mode elpy exec-path-from-shell expand-region fill-column-indicator findr fiplr flow-minor-mode general ghci-completion gist github-issues god-mode haskell-mode hasky-extensions hasky-stack helm helm-projectile hindent hl-line+ ht jsx-mode linum-relative macrostep magit magit-gh-pulls markdown-mode multi-web-mode multiple-cursors mustache-mode neotree nim-mode noflet phi-search projectile pug-mode purescript-mode rainbow-delimiters rjsx-mode rudel rust-mode shell-pop smartparens smex smooth-scroll smooth-scrolling solarized-theme sublimity swiper swoop travis typescript-mode typo undo-tree undohist vue-html-mode vue-mode web-mode xterm-color yaml-mode yasnippet)))
  '(projectile-completion-system (quote grizzl))
  '(require-final-newline t)
+ '(ring-bell-function (quote ignore))
  '(safe-local-variable-values
    (quote
     ((haskell-stylish-on-save)
